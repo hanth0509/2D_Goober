9704cc784bfb4e66e520c6c9af16cb381f365a64
Ha Nguyen <hanth05092004@gmail.com>
2025-09-17 22:50:57 +0700

    1.3_practice1

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,27 @@
+using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+    [SerializeField] float moveSpeed = 1f;
+    Rigidbody2D myRigidbody;
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+        myRigidbody = GetComponent<Rigidbody2D>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        myRigidbody.linearVelocity = new Vector2 (moveSpeed, 0f);
+    }
+    void OnTripperExit2D(Collider2D other)
+    {
+        moveSpeed = -moveSpeed;
+        FlipEnemyFacing(); //flip the enemy
+    }
+    void FlipEnemyFacing()
+    {
+        transform.localScale = new Vector2(-(Mathf.Sign(myRigidbody.linearVelocity.x)),1f);
+    }
+}
